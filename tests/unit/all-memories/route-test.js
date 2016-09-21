4f90febb83a8229eeb0b80a2c3b4b148ce648f75
NatasaPeic <peic.natasa@gmail.com>
2016-09-20 21:46:57 -0400

    Add tests

---
+++ tests/unit/all-memories/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:all-memories', 'Unit | Route | all memories', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
