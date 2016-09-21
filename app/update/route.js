0f240c53d284921ca110ef70a87bcf1d763b0f97
NatasaPeic <peic.natasa@gmail.com>
2016-09-21 12:32:35 -0400

    Add  route

---
+++ app/update/route.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('memory', params.id);
+  },
+
+  actions: {
+    updateMemory (data) {
+      this.get('store').findRecord('memory', data.id)
+      .then((memory) => {
+        memory.name = data.name;
+        memory.description = data.description;
+        memory.cohort = data.cohort;
+        memory.save();
+      })
+       .then(() => this.transitionTo('all-memories'));
+     },
+
+    goToMemories () {
+      this.transitionTo('all-memories');
+    },
+  },
+});
