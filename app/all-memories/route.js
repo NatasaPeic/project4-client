2f4a21bd9160a6c1770f878b45e03d682d9569f6
NatasaPeic <peic.natasa@gmail.com>
2016-09-20 21:44:47 -0400

    Render data from the API
    - each helper renders data
    - add background image

---
+++ app/all-memories/route.js
@@ -0,0 +1,14 @@
+export default Ember.Route.extend({
+  activate: function(){
+        $('body').css('background', "url('http://i.imgur.com/EPPfWf1.jpg') no-repeat center center fixed");
+        $('body').css('background-size', 'cover');
+        Ember.$('body').append()
+      },
+  deactivate: function(){
+        $('body').css('background', "none");
+
+      },
+      model() {
+        return this.get('store').findAll('memory');
+      },
+});
