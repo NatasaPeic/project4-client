6b03a9f3c433a20dd2d76d09f6592c0ed9cd6dc3
NatasaPeic <peic.natasa@gmail.com>
2016-09-25 16:46:49 -0400

    Add console.log after sign in to see data
    - if admin signs in, admin: true, otherwise, null.

--- app/auth/service.js
+++ app/auth/service.js
@@ -31,6 +31,8 @@ export default Ember.Service.extend({
       this.get('credentials').set('id', result.user.id);
       this.get('credentials').set('email', result.user.email);
       this.get('credentials').set('token', result.user.token);
+      this.get('credentials').set('admin', result.user.admin);
+      console.log(result);
     });
   },
 
