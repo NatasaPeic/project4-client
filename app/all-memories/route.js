760a527ff30e515083a3b35d651daa63b983d86f
NatasaPeic <peic.natasa@gmail.com>
2016-09-25 16:33:43 -0400

    Add custom GET request (`/memories?all=true`)
    - custom GET request for all memories by all users
    - add ajax.inject.service()
    - return data.memories
    
    - change index method on the API

--- app/all-memories/route.js
+++ app/all-memories/route.js
@@ -1,4 +1,22 @@
+// export default Ember.Route.extend({
+//   activate: function(){
+//         $('body').css('background', "url('http://i.imgur.com/EPPfWf1.jpg') no-repeat center center fixed");
+//         $('body').css('background-size', 'cover');
+//         Ember.$('body').append()
+//       },
+//   deactivate: function(){
+//         $('body').css('background', "none");
+//
+//       },
+//       model() {
+//         return this.get('store').findAll('memory');
+//       },
+// });
+
+import Ember from 'ember';
+
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
   activate: function(){
         $('body').css('background', "url('http://i.imgur.com/EPPfWf1.jpg') no-repeat center center fixed");
         $('body').css('background-size', 'cover');
@@ -8,7 +26,19 @@ export default Ember.Route.extend({
         $('body').css('background', "none");
 
       },
-      model() {
-        return this.get('store').findAll('memory');
-      },
+      // model() {
+      //   return this.get('store').findAll('memory');
+      // },
+
+model: function(params){
+    // this.set('memories', params.memory);
+    console.log(params);
+    return this.get('ajax').request(`/memories?all=true`, {  method: 'GET' })
+    .then((data) => {
+      console.log(data);
+      return data.memories;
+    });
+
+  },
+
 });
