70dc5052c0fc9d1f16fc0be53c85a9ae8476e4be
NatasaPeic <peic.natasa@gmail.com>
2016-09-25 16:53:20 -0400

    Add action `deleteMemory`

--- app/memories/route.js
+++ app/memories/route.js
@@ -1,4 +1,34 @@
-import Ember from 'ember';
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   ajax: Ember.inject.service(),
+//   activate: function(){
+//         $('body').css('background', "url('http://i.imgur.com/EPPfWf1.jpg') no-repeat center center fixed");
+//         $('body').css('background-size', 'cover');
+//         Ember.$('body').append()
+//       },
+//   deactivate: function(){
+//         $('body').css('background', "none");
+//
+//       },
+//       // model() {
+//       //   return this.get('store').findAll('memory');
+//       // },
+//
+// model: function(params){
+//     // this.set('memories', params.memory);
+//     console.log(params);
+//     return this.get('ajax').request(`/memories?all=true`, {  method: 'GET' })
+//     .then((data) => {
+//       console.log(data);
+//       return data.memories;
+//     });
+//
+//   },
+//
+// });
+
+
 
 export default Ember.Route.extend({
   activate: function(){
@@ -13,5 +43,14 @@ export default Ember.Route.extend({
       model() {
         return this.get('store').findAll('memory');
       },
+      actions: {
+
+        deleteMemory (memory) {
+          memory.destroyRecord()
+          .then(() => this.transitionTo('memories'));
+        },
+
+      },
+
 
 });
