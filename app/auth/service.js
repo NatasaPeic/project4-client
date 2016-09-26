00235943d58c8753cf260d9159a6d7d76513c2d5
NatasaPeic <peic.natasa@gmail.com>
2016-09-26 11:16:18 -0400

    Add `isAdmin`
    - get and set user.admin on sign in

--- app/auth/service.js
+++ app/auth/service.js
@@ -5,6 +5,7 @@ export default Ember.Service.extend({
   ajax: Ember.inject.service(),
   credentials: storageFor('auth'),
   isAuthenticated: Ember.computed.bool('credentials.token'),
+  isAdmin: Ember.computed.bool('credentials.admin'),
 
   signUp (credentials) {
     return this.get('ajax').post('/sign-up', {
