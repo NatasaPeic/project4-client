39f7ba2a0401db8c7c84e9e216435c92af9eec32
NatasaPeic <peic.natasa@gmail.com>
2016-09-28 17:36:21 -0400

    Add background image

--- app/admin-memories/route.js
+++ app/admin-memories/route.js
@@ -2,7 +2,7 @@
 
 export default Ember.Route.extend({
   activate: function(){
-        $('body').css('background', "url('http://i.imgur.com/EPPfWf1.jpg') no-repeat center center fixed");
+        $('body').css('background', "url('http://i.imgur.com/ZZnpGYB.jpg') no-repeat center center fixed");
         $('body').css('background-size', 'cover');
         Ember.$('body').append()
       },
