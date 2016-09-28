e0517e236b0d82b8ee7420707b9b1b6b03653abf
NatasaPeic <peic.natasa@gmail.com>
2016-09-28 18:04:46 -0400

    Add ga-button-white component

---
+++ app/components/ga-button-white/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+  isAdmin: Ember.computed.alias('auth.isAdmin'),
+
+
+});
