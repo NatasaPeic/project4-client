5a45213838f47d1e2410be3d60d22111771187ba
NatasaPeic <peic.natasa@gmail.com>
2016-09-28 17:49:44 -0400

    Add background image

--- app/application/route.js
+++ app/application/route.js
@@ -26,10 +26,7 @@ export default Ember.Route.extend({
       .then(() => {
         this.get('flashMessages').warning('You have been signed out.');
       });
-      // .catch(() => {
-      //   this.get('flashMessages')
-      //   .danger('There was a problem. Are you sure you\'re signed-in?');
-      // });
+  
       this.store.unloadAll();
     },
 
