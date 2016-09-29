c3af6c0de7226ea866b1b4d1862fffbe7fb2414f
NatasaPeic <peic.natasa@gmail.com>
2016-09-29 12:29:19 -0400

    route cleanup

--- app/about/route.js
+++ app/about/route.js
@@ -1,12 +1,14 @@
 export default Ember.Route.extend({
+  // when router enters the route
   activate: function(){
-        $('body').css('background', "url('http://i.imgur.com/xguLuoI.jpg') no-repeat center center fixed");
+        $('body').css('background', "url('http://i.imgur.com/iBfzBn5.jpg') no-repeat center center fixed");
         $('body').css('background-size', 'cover');
         Ember.$('body').append()
       },
+
+  // when router exits the route
   deactivate: function(){
         $('body').css('background', "none");
-
       },
 
 });
